090cedefd5ba734511ecc84f1901b3528d824131
johnwang7899@gmail.com <johnwang7899@gmail.com>
2022-08-09 13:24:17 -0400

    Adding comments

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 /**
- * A personal website about myself.
+ * A personal website with two mini apps, 
+ * where users can check weather or make a to-do list.
  */
 
-//Adding libraries to Javascript
+//Adding express
 const express = require("express");
 const app = express();
 const https = require("https");
+
 //Reading enviroment variables files.
 require("dotenv").config();
+
+//Google APIs
 const {google} = require('googleapis')
+//Google OAuth2
 const OAuth2 = google.auth.OAuth2
+
 
 const oauth2Client = new OAuth2(
   process.env.OAUTH_CLIENTID,
@@ -61,10 +67,12 @@ const item0 = new Item({
 
 const defaultItems = [item0];
 
+//Rendering home page
 app.get("/", (req, res) => {
   res.render("list");
 });
 
+//Renderding contact page
 app.get("/contact", (req, res) => {
   res.render("contact", {
     userName : null
@@ -72,17 +80,23 @@ app.get("/contact", (req, res) => {
 });
 
 /**
- * Functions handles GET request to render projects's page.
+ * Handles GET request to render projects's page.
+ * @param: callback to add default items to the to-do list,
+ *         or display items in the array, which is retrieved from
+ *         mongoDB database
  */
 app.get("/projects", (req, res) => {
   //Setting an array to save default item to the to-do list.
   let itemArray = [];
+
   //Finding all the documents of Item collections in the mongoDB.
   Item.find({}, function(err, result) {
     if (err) {
       console.log(err);
-    } else {
-      //Adding a default item if the found data is empty
+    } 
+    
+    else {
+      //Adding a default item if the found data is empty.
       if(result.length === 0) {
         Item.insertMany(defaultItems, function(err) {
           if(err) {
@@ -94,6 +108,7 @@ app.get("/projects", (req, res) => {
           }
         });
       }
+
       //Rendering user's end page with the to-do list and null weather info.
       res.render("projects", {
         listItem : result,
@@ -104,10 +119,12 @@ app.get("/projects", (req, res) => {
 });
 
 /**
- * Function makes an API call to openWeather to get 
- * weather's info and display it to the user via a Bootstrap modal.
+ * Makes an API call to openWeather to get weather's info 
+ * and display it to the user via a Bootstrap modal.
+ * @param: callback to get weather data from OpenWeather via an API call.
  */
 app.post("/weather", (req, res) => {
+
   //Checking if user's input is empty.
   if(req.body.cityName === "") {
     res.redirect("/projects");
@@ -119,6 +136,7 @@ app.post("/weather", (req, res) => {
     const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=";
     const apiID = "&appid=2d622426a5aac4e589b872adfa100b2d";
     const units = "&units=imperial";
+
     //Concatenating city name & apiKey to url
     const url = apiURL + cityName + apiID + units;
 
@@ -129,6 +147,7 @@ app.post("/weather", (req, res) => {
         if (data != null) {
           //Parsing the returned data into JSON format
           const weatherData = JSON.parse(data);
+
           //Checking for 404 code (invalid city name)
           if(weatherData.cod == 404) {
             res.render("projects", {
@@ -137,6 +156,7 @@ app.post("/weather", (req, res) => {
               statusCode : weatherData.cod,
             });
           }
+
           //Display weather data to user's end.
           else {
             res.render("projects", {
@@ -158,11 +178,14 @@ app.post("/weather", (req, res) => {
 });
 
 /**
- * Delete an item that has the checkbox checked
+ * Deletes an item from the to-do list that has the checkbox checked
+ * @param: callback to find an item, which is a document, 
+ *         and delete it from mongoDB.
  */
 app.post("/delete", (req, res) => {
   //Saving the itemID, which is document's name
   const itemID = req.body.checkbox;
+
   //Finding a document by its Id and delete it
   Item.findByIdAndRemove(itemID, function(err, result) {
 
@@ -178,12 +201,15 @@ app.post("/delete", (req, res) => {
 });
 
 /**
- * Function to recieve contact's info from user.
+ * Save and send contact's info via email to app's owner.
+ * @param: callback uses nodemailer to access gmail via
+ *         google Gmail API.
  */
 app.post("/contact", (req, res) => {
+  //Deconstructing user's input into variables
   const {name, email, message} = req.body;
-  console.log(name, email, message);
 
+  //Create a variable to access Gmail via OAuth2
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -196,6 +222,7 @@ app.post("/contact", (req, res) => {
     }
   });
 
+  //Adding email's info
   const mailOptions = {
     from: process.env.MAIL_USERNAME,
     to: "nghiaben7@gmail.com",
@@ -203,6 +230,7 @@ app.post("/contact", (req, res) => {
     text: message + " at " + email
   };
 
+  //Sending email to app's owner.
   transporter.sendMail(mailOptions, function(err, data) {
     if (err) {
       console.log("Error " + err);
@@ -211,6 +239,7 @@ app.post("/contact", (req, res) => {
     }
   });
 
+  //Inform user upon their infor has been well received.
   res.render("contact", {
     userName : name
   });
@@ -222,9 +251,11 @@ app.post("/contact", (req, res) => {
 app.post("/add", (req, res) => {
   const userInput = req.body.newItem;
   //Checking if user's input is null.
+
   if (userInput !== "") {
     //Creating a new item with the user input.
     const newItem = new Item({ name: req.body.newItem });
+    
     //Saving the new item into the database
     newItem.save();
   }
